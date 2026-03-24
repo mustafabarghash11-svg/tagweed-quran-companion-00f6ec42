c973074b42cea1e803aba5f9337963bd93ef44e2
mustafabarghash11-svg <mustafabarghash11@gmail.com>
2026-03-24 21:13:27 +0300

    Add files via upload

--- src/context/SettingsContext.tsx
+++ src/context/SettingsContext.tsx
@@ -1,1 +1,43 @@
+import { createContext, useContext, useState, useEffect } from "react";
 
+type SettingsType = {
+  reciter: string;
+  setReciter: (r: string) => void;
+  theme: string;
+  setTheme: (t: string) => void;
+};
+
+const SettingsContext = createContext<SettingsType | null>(null);
+
+export const SettingsProvider = ({ children }: any) => {
+  const [reciter, setReciter] = useState(
+    localStorage.getItem("reciter") || "husary"
+  );
+
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("reciter", reciter);
+  }, [reciter]);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  return (
+    <SettingsContext.Provider
+      value={{ reciter, setReciter, theme, setTheme }}
+    >
+      {children}
+    </SettingsContext.Provider>
+  );
+};
+
+export const useSettings = () => {
+  const context = useContext(SettingsContext);
+  if (!context) throw new Error("SettingsContext error");
+  return context;
+};

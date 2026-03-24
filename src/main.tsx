23c11c0d966d6a13526a582cb8303fdc0164ab8a
mustafabarghash11-svg <mustafabarghash11@gmail.com>
2026-03-24 21:50:42 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { SettingsProvider } from "./context/SettingsContext";
 
+<SettingsProvider>
+  <App />
+</SettingsProvider>
 createRoot(document.getElementById("root")!).render(<App />);

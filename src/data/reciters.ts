64a6f6f89c8b37405d10bae55d0dc3909fa9b97f
mustafabarghash11-svg <mustafabarghash11@gmail.com>
2026-03-24 21:26:34 +0300

    Add files via upload

--- src/data/reciters.ts
+++ src/data/reciters.ts
@@ -1,17 +1,1 @@
-export const reciters = [
-  {
-    id: "husary",
-    name: "الحصري",
-    baseUrl: "https://server8.mp3quran.net/husary/"
-  },
-  {
-    id: "sudais",
-    name: "السديس",
-    baseUrl: "https://server11.mp3quran.net/sds/"
-  },
-  {
-    id: "minshawi",
-    name: "المنشاوي",
-    baseUrl: "https://server10.mp3quran.net/minsh/"
-  }
-];
+export const reciters=[{id:"husary",name:"الحصري",baseUrl:"https://server8.mp3quran.net/husary/"},{id:"sudais",name:"السديس",baseUrl:"https://server11.mp3quran.net/sds/"},{id:"minshawi",name:"المنشاوي",baseUrl:"https://server10.mp3quran.net/minsh/"}];

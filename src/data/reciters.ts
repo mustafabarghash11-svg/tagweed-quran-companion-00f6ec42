902a873485171cffecfe24d9bb8bd53d32993905
mustafabarghash11-svg <mustafabarghash11@gmail.com>
2026-03-27 21:46:23 +0300

    Update reciters.ts

--- src/data/reciters.ts
+++ src/data/reciters.ts
@@ -1,9 +1,11 @@
 export const reciters = [
-  { id: 'husary', name: 'الحصري', baseUrl: 'https://server8.mp3quran.net/husary/', apiId: 'ar.husary' },
-  { id: 'sudais', name: 'السديس', baseUrl: 'https://server11.mp3quran.net/sds/', apiId: 'ar.abdulbasitmurattal' },
-  { id: 'minshawi', name: 'المنشاوي', baseUrl: 'https://server10.mp3quran.net/minsh/', apiId: 'ar.minshawi' },
-  { id: 'alafasy', name: 'العفاسي', baseUrl: 'https://server10.mp3quran.net/alafasy/', apiId: 'ar.alafasy' },
-  { id: 'abdulbasit', name: 'عبد الباسط', baseUrl: 'https://server7.mp3quran.net/basit/', apiId: 'ar.abdulbasit' },
+  { id: 'abdulbasit', name: 'عبد الباسط عبد الصمد', baseUrl: 'https://server7.mp3quran.net/basit/', apiId: 'ar.abdulbasit' },
+  { id: 'husary', name: 'محمود خليل الحصري', baseUrl: 'https://server8.mp3quran.net/husary/', apiId: 'ar.husary' },
+  { id: 'minshawi', name: 'محمد صديق المنشاوي', baseUrl: 'https://server10.mp3quran.net/minsh/', apiId: 'ar.minshawi' },
+  { id: 'alafasy', name: 'مشاري راشد العفاسي', baseUrl: 'https://server10.mp3quran.net/alafasy/', apiId: 'ar.alafasy' },
+  { id: 'sudais', name: 'عبد الرحمن السديس', baseUrl: 'https://server11.mp3quran.net/sds/', apiId: 'ar.sudais' },
+  { id: 'ghamdi', name: 'سعد الغامدي', baseUrl: 'https://server12.mp3quran.net/ghamdi/', apiId: 'ar.ghamdi' },
+  { id: 'ayyoub', name: 'محمد أيوب', baseUrl: 'https://server12.mp3quran.net/ayoub/', apiId: 'ar.ayyoub' },
 ];
 
 export type Reciter = typeof reciters[0];
